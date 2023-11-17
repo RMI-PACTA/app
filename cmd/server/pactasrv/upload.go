cab67c7fa7bfffcb37ef545d4c1969bc8be79dde
Grady Ward <grady.b.ward@gmail.com>
2023-11-17 10:11:51 -0700

    Addresses Review Comments, Adds Server Stubs

---
+++ cmd/server/pactasrv/upload.go
@@ -0,0 +1,20 @@
+package pactasrv
+
+import (
+	"context"
+	"fmt"
+
+	api "github.com/RMI/pacta/openapi/pacta"
+)
+
+// Starts the process of uploading one or more portfolio files
+// (POST /portfolio-upload)
+func (s *Server) StartPortfolioUpload(ctx context.Context, request api.StartPortfolioUploadRequestObject) (api.StartPortfolioUploadResponseObject, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// Called after uploads of portfolios to cloud storage are complete.
+// (POST /portfolio-upload:complete)
+func (s *Server) CompletePortfolioUpload(ctx context.Context, request api.CompletePortfolioUploadRequestObject) (api.CompletePortfolioUploadResponseObject, error) {
+	return nil, fmt.Errorf("not implemented")
+}
