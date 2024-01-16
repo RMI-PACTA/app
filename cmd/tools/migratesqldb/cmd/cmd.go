69f4e13e7af99c287917bf71c1d4d8414f047de7
Brandon Sprague <brandon@siliconally.org>
2024-01-15 18:42:50 -0800

    Add rollback support to `migratedb` and update deps

--- cmd/tools/migratesqldb/cmd/cmd.go
+++ cmd/tools/migratesqldb/cmd/cmd.go
@@ -5,11 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
-	"github.com/Silicon-Ally/testpgx/migrate"
-	"github.com/bazelbuild/rules_go/go/tools/bazel"
+	"github.com/bazelbuild/rules_go/go/runfiles"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/spf13/cobra"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func Execute() {
@@ -63,16 +67,38 @@ var (
 		Use:   "apply",
 		Short: "Apply migrations",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			migrationsPath, err := bazel.Runfile("db/sqldb/migrations")
+			migrationsPath, err := runfiles.Rlocation("__main__/db/sqldb/migrations/0001_create_schema_migrations_history.down.sql")
 			if err != nil {
 				return fmt.Errorf("failed to get a path to migrations: %w", err)
 			}
-			migrator, err := migrate.New(migrationsPath)
+			migrationsPath = filepath.Dir(migrationsPath)
+			migrator, err := newMigrator(sqlDB, migrationsPath)
 			if err != nil {
 				return fmt.Errorf("when creating the migrator: %w", err)
 			}
-			if err := migrator.Migrate(sqlDB); err != nil {
+			if err := migrator.Up(); err != nil {
 				return fmt.Errorf("while applying the migration(s): %w", err)
+			}
+			return nil
+		},
+	}
+
+	rollbackCmd = &cobra.Command{
+		Use:   "rollback",
+		Short: "Rollback migrations",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			migrationsPath, err := runfiles.Rlocation("__main__/db/sqldb/migrations/0001_create_schema_migrations_history.down.sql")
+			if err != nil {
+				return fmt.Errorf("failed to get a path to migrations: %w", err)
+			}
+			migrationsPath = filepath.Dir(migrationsPath)
+			migrator, err := newMigrator(sqlDB, migrationsPath)
+			if err != nil {
+				return fmt.Errorf("when creating the migrator: %w", err)
+			}
+			if err := migrator.Steps(-1); err != nil {
+				return fmt.Errorf("while rolling back the migration(s): %w", err)
+
 			}
 			return nil
 		},
@@ -81,5 +107,28 @@ var (
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&dsn, "dsn", "", "A Postgres DSN, parsable by pgx.ParseConfig")
-	rootCmd.AddCommand(applyCmd)
+	rootCmd.AddCommand(applyCmd, rollbackCmd)
+}
+
+func newMigrator(db *sql.DB, migrationsPath string) (*migrate.Migrate, error) {
+	// Pings the database to distinguish between migration and connection errors
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	driver, err := pgx.WithInstance(db, &pgx.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to init driver instance: %w", err)
+	}
+
+	mgr, err := migrate.NewWithDatabaseInstance(
+		"file://"+migrationsPath,
+		"pgx",
+		driver,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to init migrate instance: %w", err)
+	}
+
+	return mgr, nil
 }
