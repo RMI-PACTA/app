8d8c66ac72e2b63ff5769a8cdb01485b5bf88996
Grady Berry Ward <grady@siliconally.org>
2024-01-18 09:14:47 -0700

    Barebones Analysis Display + Audit Logs (#144)

--- reportsrv/reportsrv_test.go
+++ reportsrv/reportsrv_test.go
@@ -2,6 +2,7 @@ package reportsrv
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -11,29 +12,56 @@ import (
 	"github.com/RMI/pacta/blob"
 	"github.com/RMI/pacta/db"
 	"github.com/RMI/pacta/pacta"
+	"github.com/RMI/pacta/session"
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap/zaptest"
 )
 
 func TestServeReport(t *testing.T) {
-	ctx := context.WithValue(context.Background(), chi.RouteCtxKey, &chi.Context{
-		URLParams: chi.RouteParams{
-			Keys:   []string{"analysis_id"},
-			Values: []string{"analysis.id1"},
-		},
-	})
 
 	srv, env := setup(t)
+	router := chi.NewRouter()
+	srv.RegisterHandlers(router)
+
+	aIDStr := "analysis.id1"
+	analysisID := pacta.AnalysisID(aIDStr)
+	ownerID := pacta.OwnerID("owner.id1")
+	userID := pacta.UserID("user.id1")
+	otherUserID := pacta.UserID("user.id2")
+	adminUserID := pacta.UserID("user.id3")
+
+	env.db.users = []*pacta.User{{
+		ID: userID,
+	}, {
+		ID: otherUserID,
+	}, {
+		ID:    adminUserID,
+		Admin: true,
+	}}
+
+	env.db.userToOwner = map[pacta.UserID]pacta.OwnerID{
+		userID:      ownerID,
+		otherUserID: "other.owner.id",
+		adminUserID: "admin.owner.id",
+	}
+
+	env.db.analyses = []*pacta.Analysis{
+		&pacta.Analysis{
+			ID:    analysisID,
+			Owner: &pacta.Owner{ID: ownerID},
+		},
+	}
 
 	env.db.analysisArtifacts = []*pacta.AnalysisArtifact{
 		&pacta.AnalysisArtifact{
-			ID:         "analysisartifact.id1",
-			AnalysisID: "analysis.id1",
-			Blob:       &pacta.Blob{ID: "blob.id1"},
+			ID:                "analysisartifact.id1",
+			AnalysisID:        analysisID,
+			Blob:              &pacta.Blob{ID: "blob.id1"},
+			AdminDebugEnabled: true,
 		},
 		&pacta.AnalysisArtifact{
 			ID:         "analysisartifact.id2",
-			AnalysisID: "analysis.id1",
+			AnalysisID: analysisID,
 			Blob:       &pacta.Blob{ID: "blob.id2"},
 		},
 	}
@@ -60,36 +88,94 @@ func TestServeReport(t *testing.T) {
 		"test://reports/1111-2222-3333-4444/lib/some/package.js": jsContent,
 	}
 
-	serveReportAndCheckResponse := func(path, contentType, responseBody string) {
-		t.Run(path, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodGet, path, nil).WithContext(ctx)
+	standardPath := "/report/" + aIDStr + "/"
+	cases := []struct {
+		asUser          pacta.UserID
+		path            string
+		wantErr         int
+		wantContentType string
+		wantRespContent string
+	}{{
+		asUser:  userID,
+		path:    "/report/" + aIDStr,
+		wantErr: http.StatusTemporaryRedirect,
+	}, {
+		asUser:          userID,
+		path:            standardPath,
+		wantContentType: "text/html",
+		wantRespContent: htmlContent,
+	}, {
+		asUser:          userID,
+		path:            "/report/" + aIDStr + "/index.html",
+		wantContentType: "text/html",
+		wantRespContent: htmlContent,
+	}, {
+		asUser:          userID,
+		path:            "/report/" + aIDStr + "/lib/some/package.js",
+		wantContentType: "text/javascript",
+		wantRespContent: jsContent,
+	}, {
+		asUser:          "",
+		path:            standardPath,
+		wantContentType: "text/html",
+		wantErr:         http.StatusUnauthorized,
+	}, {
+		asUser:          otherUserID,
+		path:            standardPath,
+		wantContentType: "text/html",
+		wantErr:         http.StatusUnauthorized,
+	}, {
+		asUser:          adminUserID,
+		path:            standardPath,
+		wantContentType: "text/html",
+		wantRespContent: htmlContent,
+	}, {
+		asUser:          userID,
+		path:            "/report/a-nonsense-report/",
+		wantContentType: "text/html",
+		wantErr:         http.StatusNotFound,
+	}}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			ctx := context.WithValue(context.Background(), chi.RouteCtxKey, &chi.Context{
+				URLParams: chi.RouteParams{
+					Keys:   []string{"analysis_id"},
+					Values: []string{"analysis.id1"},
+				},
+			})
+			if c.asUser != "" {
+				ctx = session.WithUserID(ctx, c.asUser)
+			}
+
+			r := httptest.NewRequest(http.MethodGet, c.path, nil).WithContext(ctx)
 			w := httptest.NewRecorder()
 
-			srv.serveReport(w, r)
+			router.ServeHTTP(w, r)
 
 			res := w.Result()
 			// Successful response...
-			if got, want := w.Code, http.StatusOK; got != want {
-				t.Errorf("got status code %d, want %d", got, want)
+			wantCode := http.StatusOK
+			if c.wantErr != 0 {
+				wantCode = c.wantErr
+			}
+			gotCode := res.StatusCode
+			if gotCode != wantCode {
+				t.Errorf("got status code %d, want %d", gotCode, wantCode)
+			}
+			if c.wantErr != 0 {
+				return
 			}
 			// ...with the correct content-type...
-			if got, want := res.Header.Get("Content-Type"), contentType; got != want {
+			if got, want := res.Header.Get("Content-Type"), c.wantContentType; got != want {
 				t.Errorf("Content-Type = %q, want %q", got, want)
 			}
 			// ...and the correct response body.
-			if got, want := w.Body.String(), responseBody; got != want {
+			if got, want := w.Body.String(), c.wantRespContent; got != want {
 				t.Errorf("Response body = %q, want %q", got, want)
 			}
 		})
 	}
-
-	// First, request the root in a few different ways. All of then should return the /index.html file contents.
-	serveReportAndCheckResponse("/report/analysis.id1", "text/html", htmlContent)
-	serveReportAndCheckResponse("/report/analysis.id1/", "text/html", htmlContent)
-	serveReportAndCheckResponse("/report/analysis.id1/index.html", "text/html", htmlContent)
-
-	// Now, try loading an asset from the report.
-	serveReportAndCheckResponse("/report/analysis.id1/lib/some/package.js", "text/javascript", jsContent)
 }
 
 type testEnv struct {
@@ -119,10 +205,14 @@ type testDB struct {
 	// Recording inputs
 	gotAnalysisIDs []pacta.AnalysisID
 	gotBlobIDs     [][]pacta.BlobID
+	gotAuditLogs   []pacta.AuditLog
 
 	// Hardcoded outputs
+	analyses          []*pacta.Analysis
 	analysisArtifacts []*pacta.AnalysisArtifact
 	blobs             map[pacta.BlobID]*pacta.Blob
+	userToOwner       map[pacta.UserID]pacta.OwnerID
+	users             []*pacta.User
 }
 
 func (tdb *testDB) NoTxn(ctx context.Context) db.Tx {
@@ -137,6 +227,37 @@ func (tdb *testDB) AnalysisArtifactsForAnalysis(tx db.Tx, id pacta.AnalysisID) (
 func (tdb *testDB) Blobs(tx db.Tx, ids []pacta.BlobID) (map[pacta.BlobID]*pacta.Blob, error) {
 	tdb.gotBlobIDs = append(tdb.gotBlobIDs, ids)
 	return tdb.blobs, nil
+}
+
+func (tdb *testDB) GetOwnerForUser(tx db.Tx, userID pacta.UserID) (pacta.OwnerID, error) {
+	ownerID, ok := tdb.userToOwner[userID]
+	if !ok {
+		return "", fmt.Errorf("user %q not found", userID)
+	}
+	return ownerID, nil
+}
+
+func (tdb *testDB) CreateAuditLog(tx db.Tx, log *pacta.AuditLog) (pacta.AuditLogID, error) {
+	tdb.gotAuditLogs = append(tdb.gotAuditLogs, *log)
+	return pacta.AuditLogID("auditlog.id1"), nil
+}
+
+func (tdb *testDB) User(tx db.Tx, id pacta.UserID) (*pacta.User, error) {
+	for _, u := range tdb.users {
+		if u.ID == id {
+			return u, nil
+		}
+	}
+	return nil, fmt.Errorf("user %q not found", id)
+}
+
+func (tdb *testDB) Analysis(tx db.Tx, id pacta.AnalysisID) (*pacta.Analysis, error) {
+	for _, a := range tdb.analyses {
+		if a.ID == id {
+			return a, nil
+		}
+	}
+	return nil, fmt.Errorf("analysis %q not found", id)
 }
 
 type testBlob struct {
