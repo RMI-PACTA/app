f6af42be93df88b27be4a299639b897d6aa58cdc
Grady Ward <grady.b.ward@gmail.com>
2024-01-24 06:43:09 -0700

    Addresses Review Comments

--- frontend/pages/admin/merge.vue
+++ frontend/pages/admin/merge.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { useConfirm } from 'primevue/useconfirm'
+
 const pactaClient = usePACTA()
 const { loading: { withLoading } } = useModal()
 const localePath = useLocalePath()
+const { require: confirm } = useConfirm()
 
 const prefix = 'admin/merge'
 
@@ -9,12 +12,33 @@ const fromUserId = useState<string>(`${prefix}.fromUserId`, () => '')
 const toUserId = useState<string>(`${prefix}.toUserId`, () => '')
 const done = useState<boolean>(`${prefix}.done`, () => false)
 
-const doMerge = () => withLoading(() => pactaClient.mergeUsers({
-  fromUserId: fromUserId.value,
-  toUserId: toUserId.value,
-}).then((resp) => {
-  done.value = true
-}), `${prefix}.doMerge`)
+// TODO(#168) An example of the error here.
+const doMerge = (): void => {
+  void withLoading(() => pactaClient.mergeUsers({
+    fromUserId: fromUserId.value,
+    toUserId: toUserId.value,
+  }).then((resp) => {
+    done.value = true
+  }), `${prefix}.doMerge`)
+}
+
+const clickMerge = () => {
+  confirm({
+    header: 'Are you 100% sure?',
+    message: 'This will transfer all assets from the source user to the destination user, and then delete the source user. This cannot be undone. Only proceed if you have tripple checked the user IDs and are confident in this procedure.',
+    icon: 'pi pi-user-minus',
+    position: 'center',
+    blockScroll: true,
+    reject: () => { /* noop */ },
+    rejectLabel: 'Cancel',
+    rejectIcon: 'pi pi-times',
+    rejectClass: 'p-button-secondary p-button-text',
+    acceptClass: 'p-button-danger',
+    acceptLabel: 'Proceed',
+    accept: doMerge,
+    acceptIcon: 'pi pi-check',
+  })
+}
 
 const reset = () => {
   fromUserId.value = ''
@@ -40,7 +64,7 @@ const reset = () => {
         <LinkButton
           class="p-button-secondary p-button-text"
           icon="pi pi-external-link"
-          :to="localePath(`/user/${fromUserId}/edit`)"
+          :to="fromUserId ? localePath(`/user/${fromUserId}/edit`) : undefined"
           new-tab
         />
       </div>
@@ -58,7 +82,7 @@ const reset = () => {
         <LinkButton
           class="p-button-secondary p-button-text"
           icon="pi pi-external-link"
-          :to="localePath(`/user/${toUserId}/edit`)"
+          :to="toUserId ? localePath(`/user/${toUserId}/edit`) : undefined"
           new-tab
         />
       </div>
@@ -68,7 +92,7 @@ const reset = () => {
       label="Perform Merge"
       class="p-button-danger"
       icon="pi pi-user-minus"
-      @click="doMerge"
+      @click="clickMerge"
     />
     <PVMessage
       v-if="done"
